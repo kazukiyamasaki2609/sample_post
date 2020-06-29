2eb59bb68f3438fae27c4ffb4364ff76e283acee
EC2 Default User <ec2-user@ip-172-31-59-229.ec2.internal>
2020-06-29 10:53:01 +0000

    signup

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get users_path
+    assert_response :success
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+
+  
+
+end
